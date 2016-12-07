e876f48c4d20781c5e3a80b607b9ca947807b863
afjk <afjk01@gmail.com>
2016-12-07 17:13:16 +0900

    new.

---
+++ Assets/MazeMaker/Scripts/ClickToGrowMaze.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToGrowMaze : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject obj = getClickMazeMakerGrowObject();
+        if( obj)
+        {
+            Debug.Log( obj.name );
+
+            MazeMakerGrowInObj mmgo = obj.GetComponent<MazeMakerGrowInObj>();
+            if (mmgo)
+            {
+                Debug.Log("MazeMakerGrowInObj Grow!");
+                mmgo.Grow();
+            }
+        }
+    }
+
+    public GameObject getClickMazeMakerGrowObject()
+    {
+        GameObject result = null;
+
+        RaycastHit[] hits;
+        List<GameObject> gameObjects = new List<GameObject>();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            hits = Physics.RaycastAll(ray, 100.0F);
+
+            foreach (RaycastHit hit in hits)
+            {
+                MazeMakerGrowInObj mmgo = hit.collider.gameObject.GetComponent<MazeMakerGrowInObj>();
+                if (mmgo)
+                {
+                    gameObjects.Add(hit.collider.gameObject);
+                }
+            }
+
+            if (gameObjects.Count > 0)
+            {
+                result = gameObjects[0];
+                float minDistance = Vector3.Distance(Camera.main.transform.position, result.transform.position);
+
+                for (int i = 1; i < gameObjects.Count; i++)
+                {
+                    float distance = Vector3.Distance(Camera.main.transform.position, gameObjects[i].transform.position);
+                    if (minDistance < distance)
+                    {
+                        minDistance = distance;
+                        result = gameObjects[i];
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ Assets/MazeMaker/Scripts/MazeMakerGrowInObj.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeMakerGrowInObj : MonoBehaviour {
+
+    public bool growOnStart = true;
+
+    public GameObject roadObject = null;
+    public GameObject coverObject = null;
+    public GameObject preRoadObject = null;
+
+    private GameObject mMazeBase;
+
+    private bool makeMazeComplete = false;
+
+    private bool isActive = false;
+
+    public delegate void MazeMakerEventHandler(object sender);
+    public event MazeMakerEventHandler MakeComplete;
+
+    // Use this for initialization
+    void Start()
+    {
+        MakeRayList();
+
+        if ( isStart() )
+        {
+            if (coverObject == null)
+            {
+                Collider[] colliders = Physics.OverlapBox(transform.position, new Vector3(0.5f, 0.5f, 0.5f));
+
+                foreach (Collider item in colliders)
+                {
+                    if (item.gameObject != this.gameObject)
+                    {
+                        coverObject = item.gameObject;
+                        break;
+                    }
+                }
+
+            }
+
+        }
+
+        if (growOnStart)
+        {
+            Grow();
+        }
+    }
+
+    private List<Ray> mRayList = new List<Ray>();
+
+    private void MakeRayList()
+    {
+        mRayList.Add(new Ray(transform.position, this.transform.forward));
+        mRayList.Add(new Ray(transform.position, -this.transform.forward));
+        mRayList.Add(new Ray(transform.position, this.transform.right));
+        mRayList.Add(new Ray(transform.position, -this.transform.right));
+        mRayList.Add(new Ray(transform.position, this.transform.up));
+        mRayList.Add(new Ray(transform.position, -this.transform.up));
+    }
+
+    public void Grow()
+    {
+        isActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            isActive = false;
+
+            // 進行可能な方向を得る
+            Vector3 direction = GetGoDirection();
+
+            if (direction == Vector3.zero)
+            {
+                // 進行可能な方向がない場合、戻る。
+                GoBack();
+            }
+            else
+            {
+                // 進行可能な方向に2セル進む
+                GoAhead(direction);
+            }
+        }
+    }
+
+    bool InCoverObj(Vector3 pos)
+    {
+        bool result = false;
+
+        if (coverObject)
+        {
+            Collider[] colliders = Physics.OverlapBox(pos, new Vector3(0.1f, 0.1f, 0.1f));
+            foreach (Collider item in colliders)
+            {
+                if (item.gameObject == coverObject)
+                {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            result = true;
+        }
+        return result;
+    }
+
+    Vector3 GetGoDirection()
+    {
+        Vector3 result = Vector3.zero;
+
+        List<Ray> rayList = new List<Ray>();
+        RaycastHit hit;
+        float distance = 2.0f;
+        float radious = 1.0f;
+
+        if( mRayList.Count > 0 )
+        {
+            foreach (Ray ray in mRayList)
+            {
+                if (!Physics.SphereCast(ray, radious, out hit, distance))
+                {
+                    if (InCoverObj(transform.position + ray.direction * distance))
+                    {
+                        rayList.Add(ray);
+                    }
+                }
+            }
+        }
+        if (rayList.Count > 0)
+        {
+            int randIndex = Random.Range(0, rayList.Count);
+            result = rayList[randIndex].direction;
+        }
+        Debug.Log(result);
+        return result;
+    }
+
+    bool isStart()
+    {
+        if (preRoadObject == null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void GoBack()
+    {
+        Debug.Log("GoBack:in");
+        // 2セル戻る
+        // スタート地点なら生成終了
+        if (isStart())
+        {
+            Debug.Log("Maze Generate Complete!");
+            makeMazeComplete = true;
+            // 完了通知
+            OnMakeComplete();
+            return;
+        }
+        else
+        {
+            // 戻る
+            if (preRoadObject != null)
+            {
+                preRoadObject.GetComponent<MazeMakerGrowInObj>().Grow();
+            }
+        }
+    }
+
+    void GoAhead(Vector3 direction)
+    {
+        GameObject copySpaceCube1 = Object.Instantiate(roadObject) as GameObject;
+        copySpaceCube1.transform.position = this.transform.position + direction * 1.0f;
+
+        Destroy(copySpaceCube1.GetComponent<MazeMakerGrowInObj>());
+
+        GameObject copySpaceCube2 = Object.Instantiate(roadObject) as GameObject;
+        copySpaceCube2.transform.position = this.transform.position + direction * 2.0f;
+
+        copySpaceCube2.GetComponent<MazeMakerGrowInObj>().preRoadObject = this.gameObject;
+        copySpaceCube2.GetComponent<MazeMakerGrowInObj>().Grow();
+
+        if( this.transform.parent != null )
+        {
+            copySpaceCube1.transform.parent = this.transform.parent;
+            copySpaceCube2.transform.parent = this.transform.parent;
+        }
+    }
+
+    /* 迷路生成完了通知 */
+    public virtual void OnMakeComplete()
+    {
+        if (MakeComplete != null)
+        {
+            MakeComplete(this);
+        }
+    }
+}
